422f173fd4df17adc6afcde15028d2249e68ed8e
Dean Radcliffe <deanmisc@yahoo.com>
2017-01-24 21:08:51 -0600

    fix: gulp coverage works

--- src/antares.js
+++ src/antares.js
@@ -16,57 +16,67 @@ export const AntaresInit = (AntaresConfig) => {
   ViewReducer.push(AntaresConfig.ViewReducer)
   NewId.push(AntaresConfig.newId)
   ReducerForKey.push(AntaresConfig.ReducerForKey)
-  MetaEnhancers.push(...AntaresConfig.MetaEnhancers)
+  MetaEnhancers.push(...(AntaresConfig.MetaEnhancers || []))
 
   // Construct the store for this Agent!
   const store = initializeStore()
-
   // dispatcher is a location-unaware function to dispatch and return a Promise
   // Should accept an intent, and return a Promise for an ACK
   let dispatcher
 
-  // on the client define the endpoint for server communication
-  const userDispatchProxy = AntaresConfig.defineDispatchProxy()
-  const dispatchProxy = action => {
-    // Withhold localOnly actions from the wire
-    if (action.meta && action.meta.antares && action.meta.antares.localOnly) {
-      return
+
+  if (!AntaresConfig.defineDispatchProxy ||
+      !AntaresConfig.defineDispatchEndpoint ||
+      !AntaresConfig.defineRemoteActionsProducer ||
+      !AntaresConfig.defineRemoteActionsConsumer
+  ) {
+    dispatcher = () => console.error('Antares: running without full config')
+  } else {
+
+
+    // on the client define the endpoint for server communication
+    const userDispatchProxy = AntaresConfig.defineDispatchProxy()
+    const dispatchProxy = action => {
+      // Withhold localOnly actions from the wire
+      if (action.meta && action.meta.antares && action.meta.antares.localOnly) {
+        return
+      }
+      return userDispatchProxy(action)
     }
-    return userDispatchProxy(action)
+
+    // The dispatch endpoint on the server does
+    // 1) dispatch action to the store
+    // 2) notify other users
+    inAgencyRun('server', () => {
+      const dispatchEndpoint = AntaresConfig.defineDispatchEndpoint(store)
+      dispatcher = intent => {
+        return new Promise(resolve => dispatchEndpoint.call(null, intent))
+      }
+    })
+
+    // The client side version must call the server, but only after first
+    // synchronously reducing it into its own store, validating it.
+    inAgencyRun('client', () => {
+      DispatchProxy.push(dispatchProxy)
+
+      // when Antares.announce is called on the client
+      dispatcher = intent => {
+        store.dispatch(intent)
+        return dispatchProxy(intent)
+      }
+    })
+
+    // Provide the publication endpoint
+    inAgencyRun('server', () => {
+      AntaresConfig.defineRemoteActionsProducer(store)
+    })
+
+    // Ensure we're listening for remoteActions and applying them to our store
+    inAgencyRun('client', () => {
+      const remoteAction$ = AntaresConfig.defineRemoteActionsConsumer()
+      remoteAction$.subscribe(action => store.dispatch(action))
+    })
   }
-
-  // The dispatch endpoint on the server does
-  // 1) dispatch action to the store
-  // 2) notify other users
-  inAgencyRun('server', () => {
-    const dispatchEndpoint = AntaresConfig.defineDispatchEndpoint(store)
-    dispatcher = intent => {
-      return new Promise(resolve => dispatchEndpoint.call(null, intent))
-    }
-  })
-
-  // The client side version must call the server, but only after first
-  // synchronously reducing it into its own store, validating it.
-  inAgencyRun('client', () => {
-    DispatchProxy.push(dispatchProxy)
-
-    // when Antares.announce is called on the client
-    dispatcher = intent => {
-      store.dispatch(intent)
-      return dispatchProxy(intent)
-    }
-  })
-
-  // Provide the publication endpoint
-  inAgencyRun('server', () => {
-    AntaresConfig.defineRemoteActionsProducer(store)
-  })
-
-  // Ensure we're listening for remoteActions and applying them to our store
-  inAgencyRun('client', () => {
-    const remoteAction$ = AntaresConfig.defineRemoteActionsConsumer()
-    remoteAction$.subscribe(action => store.dispatch(action))
-  })
 
   const subscribeRenderer = (renderer, opts, alternateStream) => {
     // default to sync mode
@@ -115,7 +125,6 @@ export const AntaresInit = (AntaresConfig) => {
     getState: () => store.getState().antares,
     getViewState: () => store.getState().view,
     Rx,
-    dispatchProxy,
     Config: { Agents }
   }
 
