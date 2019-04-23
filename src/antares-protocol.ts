6c70a7be901dd111078afd12aa6aa290956bf21e
Dean Radcliffe <dradcliffe@g2crowd.com>
2019-04-22 23:50:19 -0500

    fix: 3.3.3 dep upgrade

--- src/antares-protocol.ts
+++ src/antares-protocol.ts
@@ -53,7 +53,7 @@ export { startWith, last, filter, delay, map, mapTo, scan } from "rxjs/operators
  */
 export class Agent implements ActionProcessor {
   public static configurableProps = ["agentId", "relayActions"]
-  public static VERSION = "3.3.2"
+  public static VERSION = "3.3.3"
 
   /**
    * The heart and circulatory system of an Agent is `action$`, its action stream. */
